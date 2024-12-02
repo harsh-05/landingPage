79e8bc5294d78a5ac48a6f87a32d3f7d0b77ae8f
harsh-05 <harshverma09022001@gmail.com>
2024-12-03 00:37:30 +0530

    changes in a button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button onClick={()=>setCount(count+1)}>Hello{count}</button>
+      <button onClick={()=>setCount(count+1)} className='bg-blue-500 rounded p-5 text-center'>Hello {count}</button>
     </>
   );
 }
