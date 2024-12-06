3037e052264539715cef9f75bfebf3863f1caf10
harsh-05 <harshverma09022001@gmail.com>
2024-12-06 23:18:52 +0530

    necessary changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <main>
-      <h1 className='text-5xl text-orange-500 font-bold font-robert-medium text-violet-300'>Welcome to Awards</h1>
+      <h1 className='text-5xl font-bold font-robert-medium text-violet-300'>Welcome to Awards</h1>
    </main>
   );
 }
