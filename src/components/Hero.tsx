b8db57edbc1a4cfe1bba432c0c44243a47375ead
harsh-05 <harshverma09022001@gmail.com>
2024-12-07 23:40:18 +0530

    added utitlities layer and components folder

---
+++ src/components/Hero.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Hero() { 
+    return <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div id='video-frame' className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75"></div>
+    </div>
+}
